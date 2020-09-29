b33ec4fb5384546299d1b2c52def30a00a4bce0c
jandedeuraerder-howest <jan.de.deurwaerder@howest.be>
2020-09-29 09:16:54 +0200

    Namespace adjusted

--- Prb.SalePriceCalculator.WPF/MainWindow.xaml.cs
+++ Prb.SalePriceCalculator.WPF/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Methoden5.SalePriceCalculator.WPF
+namespace Prb.SalePriceCalculator.WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
